112b3b0f68e95b23d1ddbd491382482886f5b01f
Danilo da Conceição Nascimento <daconnas.dcn@gmail.com>
2023-08-21 13:04:36 -0300

    test: criando testes básicos do servidor WSGI

---
+++ tests/test_application.py
@@ -0,0 +1,12 @@
+from importlib import import_module
+from unittest import TestCase
+
+from django.core.handlers.wsgi import WSGIHandler
+
+
+class TestDjangoApplication(TestCase):
+    def test_application_has_wsgi_module(self):
+        wsgi = import_module('clube_saber.wsgi')
+        application = wsgi.application
+
+        self.assertIsInstance(application, WSGIHandler)
