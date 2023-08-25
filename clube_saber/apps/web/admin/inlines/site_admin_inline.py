292a8fcde9d204fd141d907ffd91bc239eebaa8e
Danilo da Conceição Nascimento <daconnas.dcn@gmail.com>
2023-08-25 13:51:26 -0300

    feat: adicionando admin simples para contato e site

--- clube_saber/apps/web/admin/inlines/site_admin_inline.py
+++ clube_saber/apps/web/admin/inlines/site_admin_inline.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from clube_saber.apps.web.models.site import SiteSocialMedia
+from clube_saber.apps.web.models.site import GradientSiteTheme, SiteSocialMedia
 
 
 class SiteSocialMediaInlineAdmin(admin.TabularInline):
     model = SiteSocialMedia
+    extra = 0
+
+
+class GradientSiteThemeInlineAdmin(admin.TabularInline):
+    model = GradientSiteTheme
     extra = 0
--- clube_saber/apps/web/admin/site_admin.py
+++ clube_saber/apps/web/admin/site_admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
 from clube_saber.apps.web.admin.inlines.site_admin_inline import (
+    GradientSiteThemeInlineAdmin,
     SiteSocialMediaInlineAdmin,
 )
 
 
 class SiteModelAdmin(admin.ModelAdmin):
     inlines = [SiteSocialMediaInlineAdmin]
+
+
+class SiteThemeModelAdmin(admin.ModelAdmin):
+    inlines = [GradientSiteThemeInlineAdmin]
