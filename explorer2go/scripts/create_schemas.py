0106df024982a4a0fd77ca4c5cb725a8c53a2094
Danilo da Conceição Nascimento <daconnas.dcn@gmail.com>
2023-09-07 15:31:45 -0300

    chore: adicionando script para criar superusuário

--- explorer2go/scripts/create_schemas.py
+++ explorer2go/scripts/create_schemas.py
@@ -1,7 +1,15 @@
 from explorer2go.apps.tenant.models import Domain, Tenant
+from django.contrib.auth.models import User
+
+
+User.objects.create_superuser(  # type: ignore
+    username='test', password='123', email='test@test.com'
+)
+
 
 TENANTS = [
-    ('clubesaber.com', 'explorer2go', 'Clube Saber'),
+    ('clubesaber.com', 'clubesaber', 'Clube Saber'),
+    ('localhost', 'localhost', 'Clube Saber'),
 ]
 
 for domain_url, schema, name in TENANTS:
