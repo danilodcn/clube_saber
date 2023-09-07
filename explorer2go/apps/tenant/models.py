fe1bae77dae1c0385d70e61786e63931d8712be4
Danilo da Conceição Nascimento <daconnas.dcn@gmail.com>
2023-09-07 15:32:12 -0300

    feat: migrações iniciais

--- explorer2go/apps/tenant/models.py
+++ explorer2go/apps/tenant/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 from django_tenants.models import DomainMixin, TenantMixin
+from django.conf import settings
 
 
 class Tenant(TenantMixin):
     name = models.CharField('Nome', max_length=100)
-
+    favicon = models.ImageField('Ícone', null=True, blank=False)
+    manager = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        models.PROTECT,
+        verbose_name="Gerente",
+        null=True,
+        blank=False
+    )
     created_at = models.DateTimeField('Data de Criação', auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
 
