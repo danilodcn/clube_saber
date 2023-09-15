3c90c46a934ed8010b07659d43ebc21d9a5872f3
Danilo da Conceição Nascimento <31135098+danilodcn@users.noreply.github.com>
2023-09-15 08:33:45 -0300

    fix: removendo `location` do storage

--- explorer2go/apps/tenant/storage.py
+++ explorer2go/apps/tenant/storage.py
@@ -1,9 +1,7 @@
-from django_tenants.utils import parse_tenant_config_path
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class TenantS3Storage(S3Boto3Storage):
     def get_default_settings(self):
         settings = super().get_default_settings()
-        settings['location'] = parse_tenant_config_path('daconnas-%s')
         return settings
